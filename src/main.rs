798e4831efa8934e6ee4b2dd497457b4d8412e34
Saevitus <VirAeternus@valveanticheat.com>
2017-02-24 16:41:00 +1100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main()
     println!("damn syn is gay lol");
 
     let pid = Utilities::GetProcId("Discord.exe");
-    let module = Utilities::Module::GetModule("Discord.exe", "Discord.dll");
+    let module = Utilities::Module::GetModule("Discord.exe", "Discord.exe");
 
     println!("PID: {}, Size of module: {:?}", pid, module.m_dwSize);
 
